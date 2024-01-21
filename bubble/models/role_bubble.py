71f2c942c1fc8d16e8c329608486a4d1a607057c
luciovalente <luciovalente81@gmail.com>
2024-01-21 15:59:47 +0100

    First Commit

--- bubble/models/role_bubble.py
+++ bubble/models/role_bubble.py
@@ -4,6 +4,6 @@ class RoleBubble(models.Model):
     _name = 'role.bubble'
     _description = 'Role Buble'
 
-    buble_id = fields.Many2one('buble', string='Buble')
+    bubble_id = fields.Many2one('bubble', string='Buble')
     role_id = fields.Many2one('bubble.role', string='Role')
     user_id = fields.Many2one('res.users', string='User')
