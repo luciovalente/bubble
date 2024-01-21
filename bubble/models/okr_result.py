bf6f32a8cf917c79cc1a28e36096119d000d1550
luciovalente <luciovalente81@gmail.com>
2024-01-21 23:31:32 +0100

    First Commit

--- bubble/models/okr_result.py
+++ bubble/models/okr_result.py
@@ -9,6 +9,9 @@ class OkrResult(models.Model):
     result = fields.Float(string='Result')
     result_char = fields.Char(string="Result Char")
     evaluation_id = fields.Many2one('okr.evaluation', string='OKR Evaluation')
+    bubble_id = fields.Many2one('bubble', string='Bubble',related="okr_id.bubble_id")
+    with_automation = fields.Boolean(related="okr_id.with_automation")
+    bubble_role_id = fields.Many2one('bubble.role', string='Role',related="okr_id.bubble_role_id")
     status = fields.Selection(related="evaluation_id.status",store=True)
     user_id = fields.Many2one('res.users',related="evaluation_id.user_id",store=True)
 
