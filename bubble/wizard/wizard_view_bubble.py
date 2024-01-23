a19d57ae365da2e83679e3f583f7a07e57ecd1bd
luciovalente <luciovalente81@gmail.com>
2024-01-23 15:06:06 +0100

    First Commit

--- bubble/wizard/wizard_view_bubble.py
+++ bubble/wizard/wizard_view_bubble.py
@@ -5,3 +5,11 @@ class WizardBubbleView(models.TransientModel):
     _description = 'Wizard bubble view'
 
     bubble_id = fields.Many2one('bubble', string='Bubble', required=True)
+    legenda = fields.Html(compute="_compute_legenda")
+
+    def _compute_legenda(self):
+        types = self.env['bubble.type'].sudo().search([(1,'=',1)])
+        result = "<div style='position: absolute; top: 10px; right: 10px; display: block;display:none;'>"
+        for t in types:
+            result += '<span style="with:10px;border:1px solid %s;background-color:%s" />%s</span>'%(t.css_color,t.css_color,t.name)
+        result += "</div>"
