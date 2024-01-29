592c7df10039a9c11d09ec359bb71afa60a69c4a
Lucio <luciovalente81@gmail.com>
2024-01-29 09:56:52 +0000

    Apply auto-fixes

--- bubble/tests/__init__.py
+++ bubble/tests/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 from . import (test_bubble, test_bubble_role, test_bubble_type,
-               test_evaluation_type, test_objective, test_evaluation, test_wizard,test_okr_result)
+               test_evaluation, test_evaluation_type, test_objective,
+               test_okr_result, test_wizard)
