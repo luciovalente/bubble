2cdb8ca867a8d7c16c31254ff0f9d823b6666f66
luciovalente <luciovalente81@gmail.com>
2024-01-31 20:10:32 +0100

    merge on 15

--- bubble/models/bubble.py
+++ bubble/models/bubble.py
@@ -315,7 +315,7 @@ class Bubble(models.Model):
 
     def get_bubble_json(self):
         def is_highlight(bubble):
-            if self.env.user.id in bubble.member_ids:
+            if self.env.user.id in bubble.member_ids.ids:
                 return True
             if bubble.child_bubble_ids:
                 return any(is_highlight(child) for child in bubble.child_bubble_ids)
